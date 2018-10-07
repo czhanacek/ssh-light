5928181e588ecec65787931bcde0fadcf5580c7d
Charlie Hanacek <charlie@czhanacek.com>
2018-10-07 02:05:43 -0700

    added abstract light classes

---
+++ flasher.py
@@ -0,0 +1,13 @@
+
+class EmitsLight():
+    def off(): raise NotImplementedError
+
+    def on(): raise NotImplementedError
+
+    def status(): raise NotImplementedError
+
+class Flashable():
+    def flash(): raise NotImplementedError
+    
+
+    

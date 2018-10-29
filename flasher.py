d63b30534367ef7b9d5dd78869d29bc9f1361ab9
Charlie Hanacek <charlie@czhanacek.com>
2018-10-28 19:09:34 -0700

    should work

--- flasher.py
+++ flasher.py
@@ -1,5 +1,7 @@
-
+import RPi.GPIO as GPIO
+import time
 class EmitsLight():
+    self.status = 0
     def off(): raise NotImplementedError
 
     def on(): raise NotImplementedError
@@ -10,4 +12,27 @@ class Flashable():
     def flash(): raise NotImplementedError
     
 
+class LED_Light(EmitsLight, Flashable):
+    def __init__(self, status=0, pin=2):
+        self.status = status
+        self.pin = pin
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.pin, GPIO.OUT)
+
+
+    def status(self):
+        return self.status
+
+    def off(self):
+        GPIO.output(self.pin, GPIO.LOW)
+        self.status = 0
+
     
+    def on(self):
+        GPIO.output(self.pin, GPIO.HIGH)
+        self.status = 1
+
+    def flash(self):
+        self.on()
+        time.sleep(0.5)
+        self.off()
--- receiver.py
+++ receiver.py
@@ -1,14 +1,20 @@
 import paho.mqtt.client as mqtt
 import os
+from flasher import LED_Light
 
+
+led = LED_Light()
 def on_connect(client, userdata, flags, rc):
     print("Connected")
 
 def on_message(client, userdata, msg):
     #print(msg.topic+" "+str(msg.payload))
-    print("We got a message: " + str(msg.payload))
+    if(msg.payload == "connection"):
+        led.flash()
+    
 
 def main():
+    
     client = mqtt.Client("laptop")
     client.on_connect = on_connect
     client.on_message = on_message
